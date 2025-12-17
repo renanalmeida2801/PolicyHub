100986352d43bc15bca896b9869e8af535a86f96
Herik Cosmo <herikcosmo.m@gmail.com>
2025-12-17 02:30:14 -0300

    adicionando validacoes com operadores

--- app/core/evaluator.py
+++ app/core/evaluator.py
@@ -1,6 +1,20 @@
+import operator
+
 class PolicyEvaluator:
     def __init__(self, policies: list):
         self.policies = policies
+
+        self.ops = {
+            "eq": operator.eq,
+            "ne": operator.ne,
+            "gt": operator.gt,
+            "lt": operator.lt,
+            "ge": operator.ge,
+            "le": operator.le,
+            "in": lambda x, y: x in y,
+            "contains": lambda x, y: y in x
+
+        }
     
     def evaluate(self, request):
         return self._evaluate_internal(request)
@@ -18,12 +32,24 @@ class PolicyEvaluator:
         if policy["resource"] != request.resource:
             return False
         
-        for key, value in policy["conditions"].items():
+        for key, condition in policy["conditions"].items():
             if key == "roles":
-                if not any(role in request.user.roles for role in value):
+                if not any(role in request.user.roles for role in condition):
                     return False
             else:
-                if request.context.get(key) != value:
+                req_value = request.context.get(key)
+                
+                if isinstance(condition, dict) and "operator" in condition:
+                    op_name = condition.get("operator")
+                    target_value = condition.get("value")
+
+                    op_func = self.ops.get(op_name, operator.eq)
+
+                    if not op_func(req_value, target_value):
+                        return False
+                    
+                elif req_value != condition:
                     return False
         
         return True
+
--- app/models/request_models.py
+++ app/models/request_models.py
@@ -3,7 +3,7 @@ from typing import List, Dict, Any
 
 class UserContext(BaseModel):
     id: str
-    roles: List[str]
+    roles: List[str] = []
 
 class PolicyRequest(BaseModel):
     user: UserContext
